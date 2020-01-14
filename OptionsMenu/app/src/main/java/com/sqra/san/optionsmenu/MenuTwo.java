3547a7938ea4cfd209706417258424f31cad3a87
sansqra <sanlukesqra@gmail.com>
2020-01-14 10:58:59 +0530

    options menu1

---
+++ OptionsMenu/app/src/main/java/com/sqra/san/optionsmenu/MenuTwo.java
@@ -0,0 +1,13 @@
+package com.sqra.san.optionsmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_two);
+    }
+}
